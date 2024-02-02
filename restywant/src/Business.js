140618affb689b29abda7645e2ce4c111ab45a3e
Michael Lanius <michaellanius@Michaels-MacBook-Air.local>
2024-02-01 20:12:09 -0600

    first updates

---
+++ restywant/src/Business.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from './BusinessList';
+
+// const BusinessObject = {
+//     name: 'Mike\'s Food Trough',
+//     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+//     address: '811 Somewhere Lane',
+//     city: 'Vtown',
+//     state: 'WI',
+//     zipCode: '53573',
+//     category: 'Goooooood',
+//     rating: 4.9,
+//     review_count: 385
+// };
+
+function Business() {
+    return ( 
+    <div>
+        <header><h1>Restuarants</h1></header>
+        <p>Current list of restaurants</p>
+        <ul id='left-align'><BusinessList /></ul>
+    </div>
+    )
+ };
+
+ export default Business;
